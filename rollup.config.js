3bd3620f7da9cf11b243cf1495b82d340441e183
lijuhong1981 <lijuhong@hotmail.com>
2024-04-26 15:32:49 +0800

    更新项目配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 const output = (file, format, sourcemap) => ({
